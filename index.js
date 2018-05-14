5bcedb2a90556dc7366123132308bf7a9592454f
dimpledhawan <dhawan.dimple21@gmail.com>
2018-05-14 13:50:57 -0400

    homework1 feature2

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // console.log("it works.")
-const heading = document.querySelector('h1')
+const heading = document.querySelector('#h2')
 
 const button = document.querySelector('#button1')
 button.addEventListener('click', function(f) {
