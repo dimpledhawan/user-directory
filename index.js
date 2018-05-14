a2c04f9bad0d30c78894f82df69bb84a1102c910
dimpledhawan <dhawan.dimple21@gmail.com>
2018-05-14 13:52:05 -0400

    homework1 feature3

--- index.js
+++ index.js
@@ -6,4 +6,14 @@ button.addEventListener('click', function(f) {
     heading.textContent = 'Changed!'
 })
 
+const button2 = document.querySelector('#submitButton')
+button2.addEventListener('click', function(f) {
+    console.log(heading.textContent)
+    const text = document.querySelector('#form1').elements.item(0).value
+    heading.textContent = text
+    console.log(heading.textContent)
+})
+
+console.log(heading.textContent)
+
 
